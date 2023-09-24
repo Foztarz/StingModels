f14b645f29ff2dbc06345596261211bc9c9c1733
Foztarz <jjfoster86@gmail.com>
2023-09-24 18:36:14 +0200

    Good parameters, first tests

--- StingBinom.R
+++ StingBinom.R
@@ -55,13 +55,13 @@ dt_sim = data.frame(indiv = sort(rep(x = 1:n_indiv,
 
 # Simulate curve -----------------------------------------------------------
 zero_crossing = 2#  probability for treatment 1 would reach 0.5 on the 5th trial
-pop_trial_slope = -0.9 #change in log odds with every trial
-pop_treat_slope = 1.10 #change in log odds with treatment 2 (tripled odds ≈ exp(1.1))
-pop_treat_trial_slope = 0.1 #change in trial effect with treatment 2
+pop_trial_slope = -1.20 #change in log odds with every trial (70% reduction ≈ 1-exp(-1.20))
+pop_treat_slope = 1.10 #change in log odds with treatment 2 (tripled odds ≈ exp(1.10))
+pop_treat_trial_slope = 0.30 #change in trial effect with treatment 2 (quite large)
 pop_intercept = -zero_crossing*pop_trial_slope # set intercept to match zero crossing (inflection point of sigmoid)
-ind_intercept_sd = 1.0 #standard deviation of individual intercepts (log odds)
-ind_trial_sd = 0.5 #standard deviation of individual trial slopes
-residual_sd = 0.10#small source of unnaccounted error
+ind_intercept_sd = 0.5 #standard deviation of individual intercepts (log odds)
+ind_trial_sd = 0.3 #standard deviation of individual trial slopes
+residual_sd = 0.05#small source of unnaccounted error
 # ind_treat_sd = 0.1 #standard deviation of individual treatment slopes #indistinguishable from intercept
 ind_intercepts = rnorm(n = 1:n_indiv,
                        mean = 0,
@@ -106,9 +106,9 @@ abline(v = c(zero_crossing, #original p(50%)
 
 # . Generate simulated data -----------------------------------------------
 
-#Make an empty vector
-response_y = rep(x = NA,
-                 length = n_indiv * n_treat * n_trials)
+# #Make an empty vector
+# response_y = rep(x = NA,
+#                  length = n_indiv * n_trials)
 
 dt_sim = within(dt_sim,
               {
@@ -119,51 +119,106 @@ dt_sim = within(dt_sim,
                  #scaled by the effect of successive trials
                  #plus a small additional source or error
                  
-                 #WIP SOMETHING IS WRONG HERE
                  response_y = pop_intercept + ind_intercepts[indiv] +
-                    (treat - 1) * pop_treat_slope +
-                    (trial - 1) * pop_trial_slope + ind_trial_bias[indiv] +
-                    (trial - 1) * pop_treat_trial_slope +
-                    rnorm(n = n_indiv * n_treat * n_trials,
+                    (treat-1) * pop_treat_slope +
+                    (trial) * pop_trial_slope + ind_trial_bias[indiv] +
+                    (trial) * pop_treat_trial_slope +
+                    rnorm(n = n_indiv * n_trials,
                           mean = 0,
                           sd = residual_sd)
               }
               )
-#Loop through experiment design and generate theoretical response
-for (ii in 1:length(response_y))
-{
-   #each data point is generated from the animal's intercept
-   #plus the effect of treatment
-   #plus the effect of successive trials
-   #plus the animal's response (slope) to changing treatment
-   #scaled by the effect of successive trials
-   #plus a small additional source or error
-   response_y[ii] = ind_intercepts[animal[ii]] +
-      (stype[ii] - 1) * type_mean +
-      (slope_animal[animal[ii]] +
-          (stype[ii] - 1) * type_slope * type_animal[animal[ii]]) * stimulus[ii] +
-      rnorm(n = 1,
-            mean = 0,
-            sd = residual_sd)
-}
-#convert to binomial
-correct_incorrect = rbinom(n = length(response_y),
-                           size = 1,
-                           prob = plogis(q = response_y - mean(response_y)) )
+#convert to probability and simulate binomial
+dt_sim = within(dt_sim,
+              {
+                #each response on the log-odds scale gives us
+                #response probability on a "logit" scale
+                 p_response = plogis(response_y)
+                 #that response probabilty determines the sting event
+                 #which we observe once for each trial
+                 sting = rbinom(n = n_indiv * n_trials, #total trial number
+                                size = 1, # one observation per trial
+                                prob = p_response)
+              }
+              )
+
+# . Inspect simulated data ------------------------------------------------
+
+# View(dt_sim)
+summary(dt_sim)
+
+#plot the expected probabilities for each individual (after added noise)
+plot(x = xx, 
+     y = yy_prob,
+     ylim = c(0,1),
+     type = 'l',
+     col = 2,
+     lwd = 3,
+     ylab = 'probability',
+     xlab = 'trial')
+lines(x = xx,
+      y = yy_prob2,
+      col = 7,
+      lwd = 3)
+with(subset(x = dt_sim,
+            subset = treat == 1),
+     points(x = trial,
+            y = p_response,
+            col = 2,
+            pch = 3)
+      )
+
+with(subset(x = dt_sim,
+            subset = treat == 2),
+     points(x = trial,
+            y = p_response,
+            col = 7,
+            pch = 4)
+      )
+
+abline(h = c(1,0.5,0),
+       lty = c(1,3,1))
+abline(v = c(zero_crossing, #original p(50%)
+             -(pop_intercept + pop_treat_slope) / 
+               (pop_trial_slope + pop_treat_trial_slope)), #treatment 2 p(50%)
+       lty = 3,
+       col = c(2,3))
 
 
-# Combine this into a single data.frame format object,
-# with the experimental design and the measured response data
-sim_data = data.frame(
-   correct_incorrect = correct_incorrect,
-   animal = LETTERS[animal],
-   #animal names will now be capital letters
-   stimulus = stimulus,
-   type = factor(stype,
-                 labels = c('alpha',
-                            'beta'))
-)
-View(sim_data)
+# . aggregate simulated data to summarise ---------------------------------
+dt_agg = aggregate(sting ~ trial*treat,
+                   data = dt_sim,
+                   FUN = mean)
+
+
+#plot the expected probabilities for each individual (after added noise)
+plot(x = xx, 
+     y = yy_prob,
+     ylim = c(0,1),
+     type = 'l',
+     col = 2,
+     lwd = 3,
+     ylab = 'probability',
+     xlab = 'trial')
+lines(x = xx,
+      y = yy_prob2,
+      col = 7,
+      lwd = 3)
+with(subset(x = dt_agg,
+            subset = treat == 1),
+     points(x = trial,
+            y = sting,
+            col = 2,
+            pch = 3)
+      )
+
+with(subset(x = dt_agg,
+            subset = treat == 2),
+     points(x = trial,
+            y = sting,
+            col = 7,
+            pch = 4)
+      )
 
 
 # Load packages required for mixed-effects modelling ----------------------
@@ -174,19 +229,19 @@ require(lme4)
 ctrl_opt = glmerControl(optimizer = 'bobyqa') #efficient optimiser for models with many parameters
 
 #Maximal model with random intercepts for individuals
-glmm.max = glmer(formula = correct_incorrect~
-                    stimulus * type +
-                    (1 + stimulus * type | animal),
-                 data = dta,
+glmm.max = glmer(formula = sting~
+                    treat * trial +
+                    (1 + treat * trial | indiv),
+                 data = dt_sim,
                  control = ctrl_opt,
                  family = binomial(link = 'logit')
 )
 #boundary (singular) fit: see ?isSingular
 #Some random effects are too small to estimate properly, common warning
 #Null model, with only random effects
-glmm.null = glmer(formula = correct_incorrect~
-                     1 + (1|animal),
-                  data = dta,
+glmm.null = glmer(formula = sting~
+                     1 + (1|indiv),
+                  data = dt_sim,
                   family = binomial(link = 'logit')
 )
 # Model comparison --------------------------------------------------------
@@ -195,16 +250,127 @@ glmm.null = glmer(formula = correct_incorrect~
 extractAIC(glmm.max)[2]#2nd component is the AIC, 1st is the d.f.
 extractAIC(glmm.null)[2]
 #If AIC is lower for the maximal model, then the maximal model fits
-# 685.9584 < 3587.245
+# 195.693 < 219.778
 #We can also perform a likelihood ratio test, confusingly called "anova"
 anova(glmm.max, glmm.null, test = 'Chisq')
 #This gives the same answer as the AIC, but the difference can be reported with
 #a test statistic and p-value
 ##           npar    AIC    BIC  logLik deviance  Chisq Df Pr(>Chisq)
-## glmm.null    2 3587.2 3599.1 -1791.62   3583.2                         
-## glmm.max    14  686.0  769.1  -328.98    658.0 2925.3 12  < 2.2e-16 ***
+## glmm.null    2 219.78 225.93 -107.889   215.78                            
+## glmm.max    14 195.69 238.75  -83.846   167.69 48.085 12  3.021e-06 ***
 
 #report:
-#change in deviance = 2925.3 
+#change in deviance = 48.085   
 #change in degrees of freedom = 12
-# p < 2.2e-16 (the smallest number the computer can think of)
+#p = 3.021 x 10^-06
+#(often p < 2.2e-16; the smallest number the computer can think of)
+
+# Check model reduction options -------------------------------------------
+#If we are not sure that we need all of our parameters, we might consider model reduction.
+#This is not always necessary, and without a good rationale it may be preferable
+#to use the maximal model, which controls for all possible combinations of parameters.
+
+#can we remove one or more fixed effect to improve the model fit?
+
+#effect of stimulus-type interaction
+no_int = update(object = glmm.max, 
+                .~. - treat:trial - #remove the interaction
+                  (1 + treat * trial | indiv) + #remove random effects that include it
+                  (1 + treat + trial | indiv) #replace them with random effect that exclude it
+)
+anova(glmm.max,
+      no_int)
+
+# no_int: sting ~ treat + trial + (1 + treat + trial | indiv)
+# glmm.max: sting ~ treat * trial + (1 + treat * trial | indiv)
+#           npar    AIC    BIC  logLik deviance  Chisq Df Pr(>Chisq)   
+# no_int      9 187.92 215.60 -84.962   169.92                       
+# glmm.max   14 195.69 238.75 -83.846   167.69 2.2309  5     0.8164
+#lower AIC following the parameter's removal indicates 
+#that the model without an interaction is a better fit.
+#However, the difference is not significant, so there is
+#no significant evidence for or against an interaction.
+
+#We simulated quite a substantial effect 
+#(change in log odds of 0.30, an exp(0.30) ≈ 1.35 x increase in relative sting prob per trial),
+#but it appears hard to detect in this size of dataset.
+
+
+#We can also use this method to report the strength of our fixed effects
+#we would report this as:
+#likelihood ratio test, change in deviance  = 2.2309, d.f. = 5, p = 0.8164
+
+#effect of treatment
+no_treatment = update(object = no_int, 
+                     .~. - treat - #remove the effect of treatment
+                       (1 + treat + trial | indiv) + #remove random effects that include it
+                       (1 + trial | indiv) #replace them with random effect that exclude it
+)
+anova(no_int,
+      no_treatment)
+##              npar   AIC   BIC   logLik deviance  Chisq Df Pr(>Chisq)    
+## no_treatment   5 205.79 221.17 -97.895   195.79                           
+## no_int         9 187.92 215.60 -84.962   169.92 25.866  4  3.368e-05 ***
+
+#larger AIC following the parameter's removal indicates 
+#that the model without an effect of treatment is a significantly poorer fit
+#therefore, there is a significant effect of treatment
+#we would report this as:
+#likelihood ratio test, change in deviance  = 25.866  , d.f. = 4, p < 0.001
+
+#effect of trial
+no_trial = update(object = no_treatment, 
+                 .~. - trial - #remove the effect of type
+                   (1 + trial | indiv) + #remove random effects that include it
+                   (1 | indiv) #replace them with random effect that exclude it
+)
+anova(no_treatment,
+      no_trial)
+##              npar   AIC   BIC   logLik deviance  Chisq Df Pr(>Chisq)    
+## no_trial       2 219.78 225.93 -107.889   215.78                           
+## no_treatment   5 205.79 221.17  -97.895   195.79 19.988  3  0.0001707 ***
+
+#larger AIC following the parameter's removal indicates 
+#that the model without an effect of trial is a significantly poorer fit
+#therefore, there is a significant effect of trial
+#we would report this as:
+#likelihood ratio test, change in deviance  = 19.988, d.f. = 3, p < 0.001
+
+
+# Discarded ---------------------------------------------------------------
+# #Loop through experiment design and generate theoretical response
+# for (ii in 1:length(response_y))
+# {
+#    #each data point is generated from the animal's intercept
+#    #plus the effect of treatment
+#    #plus the effect of successive trials
+#    #plus the animal's response (slope) to changing treatment
+#    #scaled by the effect of successive trials
+#    #plus a small additional source or error
+#    response_y[ii] = ind_intercepts[animal[ii]] +
+#       (stype[ii] - 1) * type_mean +
+#       (slope_animal[animal[ii]] +
+#           (stype[ii] - 1) * type_slope * type_animal[animal[ii]]) * stimulus[ii] +
+#       rnorm(n = 1,
+#             mean = 0,
+#             sd = residual_sd)
+# }
+# #convert to binomial
+# correct_incorrect = rbinom(n = length(response_y),
+#                            size = 1,
+#                            prob = plogis(q = response_y - mean(response_y)) )
+# 
+# 
+# # Combine this into a single data.frame format object,
+# # with the experimental design and the measured response data
+# sim_data = data.frame(
+#    correct_incorrect = correct_incorrect,
+#    animal = LETTERS[animal],
+#    #animal names will now be capital letters
+#    stimulus = stimulus,
+#    type = factor(stype,
+#                  labels = c('alpha',
+#                             'beta'))
+# )
+
+
